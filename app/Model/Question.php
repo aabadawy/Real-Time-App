2d8fb99744832a060c546b99221e40db1945b65a
ahmed-Badawy-fciah <ahmedbadawy.fcia@gmail.com>
2020-05-12 14:04:07 +0200

    Add  Relationships to every model

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+       return $this->belongsTo('App\User');
+    }
+
+    public function replies()
+    {
+       return $this->hasMany('App\Reply');
+    }
+
+    public function Category()
+    {
+      return $this->belongsTo('App\Category');
+    }
 }
